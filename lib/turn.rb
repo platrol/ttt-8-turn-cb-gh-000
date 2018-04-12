662140437def144cd04ca89ce9518ce138eeb842
norman lewis <takialog@gmail.com>
2018-04-12 11:14:07 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,11 +7,11 @@ def position_taken?(board, index)
 end
 
 def display_board(board)
-  puts " #{board[1]} | #{board[2]} | #{board[3]}"   
+  puts " #{board[1]} | #{board[2]} | #{board[3]}"
   puts "-----------"
-  puts " #{board[4]} | #{board[5]} | #{board[6]}"   
+  puts " #{board[4]} | #{board[5]} | #{board[6]}"
   puts "-----------"
-  puts " #{board[7]} | #{board[8]} | #{board[9]}"   
+  puts " #{board[7]} | #{board[8]} | #{board[9]}"
 end
 
 def turn(board)
