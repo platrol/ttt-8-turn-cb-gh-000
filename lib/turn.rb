844e874bfa537d0f6140a303aa9e97e4410df7f0
norman lewis <takialog@gmail.com>
2018-04-12 11:23:36 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -27,7 +27,7 @@ def turn(board)
   input = gets.strip
   index = input_to_index(input)
   if valid_move?(board, index)
-    move(board, index, player='X')
+    move(board, index)
     return   display_board(board)
   else
     turn(board)
