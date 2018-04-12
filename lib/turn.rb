39d162546971922d5a1675070f23ec29ffee1f1c
norman lewis <takialog@gmail.com>
2018-04-12 11:00:36 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,7 @@
+def valid_move?(board, index)
+  !position_taken?(board, index) && index.between?(0, 8)
+end
+
+def position_taken?(board, index)
+  !(board[index] ==  " " || board[index] == "" || board[index] == nil)
+end
