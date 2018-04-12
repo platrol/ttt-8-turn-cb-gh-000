1f4e2e7f4b72a996d80e539fb42eefce8fa88efd
norman lewis <takialog@gmail.com>
2018-04-12 11:14:00 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -5,3 +5,21 @@ end
 def position_taken?(board, index)
   !(board[index] ==  " " || board[index] == "" || board[index] == nil)
 end
+
+def display_board(board)
+  puts " #{board[1]} | #{board[2]} | #{board[3]}"   
+  puts "-----------"
+  puts " #{board[4]} | #{board[5]} | #{board[6]}"   
+  puts "-----------"
+  puts " #{board[7]} | #{board[8]} | #{board[9]}"   
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  index = gets.strip.to_i - 1
+  if valid_move(board)
+    return   display_board(board)
+  else
+    turn(board)
+  end
+end
